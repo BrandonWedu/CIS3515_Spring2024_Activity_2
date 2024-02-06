bc99dd2ed69366a83a2a74c51ac33460b4384102
@BrandonWedu <brandon.education@proton.me>
2024-02-06 12:04:08 -0500

    Change text size to the number selected in the spinner.

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -21,7 +21,26 @@ class MainActivity : AppCompatActivity() {
         spinner.adapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,numberArray)
 
         // TODO Step 3: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                parent?.run {
+                    val fontSize = getItemAtPosition(position) as Int
+                    displayTextView.textSize = fontSize.toFloat()
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                displayTextView.textSize = 0.0F
+            }
+
+
+        }
+
 
     }
 }
